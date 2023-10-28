336c8635886c983954196041a9f99ea46733f3a1
Aditya Deshmukh <adityad1405@gmail.com>
2023-10-29 01:36:34 +0530

    connect lines to points, build spinny cube

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ struct vec3{
     float x, y, z;
 };
 
+struct connection{
+    int a,b;
+};
+
 void rotate(vec3& point, float x = 1, float y = 1, float z = 1){
     float rad = 0;
     
@@ -52,6 +56,23 @@ int main(){
         {100, 200, 200}
     };
 
+    std::vector<connection> connections{
+        {0, 4},
+        {1, 5},
+        {2, 6},
+        {3, 7},
+
+        {0, 1},
+        {1, 2},
+        {2, 3},
+        {3, 0},
+
+        {4, 5},
+        {5, 6},
+        {6, 7},
+        {7, 4}
+    };
+
 
     // Calculate the Centroid of the Cube
     vec3 centroid{0, 0, 0};
@@ -71,11 +92,17 @@ int main(){
             p.x -= centroid.x;
             p.y -= centroid.y;
             p.z -= centroid.z;
-            rotate(p, 0.002, 0.001, 0.004);
+            rotate(p, 0.02, 0.01, 0.04);
             p.x += centroid.x;
             p.y += centroid.y;
             p.z += centroid.z;
             screen.pixel(p.x, p.y);
+        }
+        for(auto& conn : connections){
+            line(screen, points[conn.a].x,
+                         points[conn.a].y,
+                         points[conn.b].x,
+                         points[conn.b].y);
         }
 
         screen.show();
